495659650073bb4d761755423cb87b994c125540
Leonardo <leomarciaga31@protonmail.com>
2022-05-26 19:49:21 -0500

    Removed other countries, imported pickle

--- model.py
+++ model.py
@@ -6,14 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
+import pickle
+
 
 co2_data = pd.read_csv("owid-co2-data.csv")
-
 pan_co2 = co2_data.loc[co2_data["iso_code"] == "PAN"]
-cri_co2 = co2_data.loc[co2_data["iso_code"] == "CRI"]
-nic_co2 = co2_data.loc[co2_data["iso_code"] == "NIC"]
-hnd_co2 = co2_data.loc[co2_data["iso_code"] == "HND"]
-slv_co2 = co2_data.loc[co2_data["iso_code"] == "SLV"]
-gtm_co2 = co2_data.loc[co2_data["iso_code"] == "GTM"]
-blz_co2 = co2_data.loc[co2_data["iso_code"] == "BLZ"]
-mex_co2 = co2_data.loc[co2_data["iso_code"] == "MEX"]
